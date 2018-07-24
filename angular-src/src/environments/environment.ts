b8d3cd2ecda4b00548de19b38450ffb045ce69d2
Shubhankardubey <dubeyshubhankar007@gmail.com>
2018-07-24 17:29:03 +0530

    code changes

---
+++ angular-src/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDNzK9wIBEWSOicgd0H7Kr-JDa-ZYIChNo",
+    authDomain: "todolist-83cc8.firebaseapp.com",
+    databaseURL: "https://todolist-83cc8.firebaseio.com",
+    projectId: "todolist-83cc8",
+    storageBucket: "todolist-83cc8.appspot.com",
+    messagingSenderId: "416490403684"
+  }
+};
---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  //database: 'mongodb://shubh:shubh@ds259089.mlab.com:59089/meanauthapp12',   //prod
+  database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
